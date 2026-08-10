f67d52318b2dfc3fec8f15d0bf18158ede56c9e3
hemanthandhe <74053534+hemanthandhe@users.noreply.github.com>
2026-08-10 12:27:03 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAEfYK5nWz7NKkUlyJYmLUMrZ1gki30Wao",
-  authDomain: "speechtodo-62326.firebaseapp.com",
-  databaseURL: "https://speechtodo-62326-default-rtdb.firebaseio.com",
-  projectId: "speechtodo-62326",
-  storageBucket: "speechtodo-62326.firebasestorage.app",
-  messagingSenderId: "198151845686",
-  appId: "1:198151845686:web:27528bedcd12b0755c6597"
+    apiKey: "AIzaSyAEfYK5nWz7NKkUlyJYmLUMrZ1gki30Wao",
+    authDomain: "speechtodo-62326.firebaseapp.com",
+    databaseURL: "https://speechtodo-62326-default-rtdb.firebaseio.com",
+    projectId: "speechtodo-62326",
+    storageBucket: "speechtodo-62326.firebasestorage.app",
+    messagingSenderId: "198151845686",
+    appId: "1:198151845686:web:27528bedcd12b0755c6597"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const db = firebase.database();
+var db = firebase.database();
