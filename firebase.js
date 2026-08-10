905a148532d212a18ab2880e9fca5341baef11e0
hemanthandhe <74053534+hemanthandhe@users.noreply.github.com>
2026-08-10 12:21:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAEfYK5nWz7NKkUlyJYmLUMrZ1gki30Wao",
   authDomain: "speechtodo-62326.firebaseapp.com",
@@ -13,10 +5,9 @@ const firebaseConfig = {
   projectId: "speechtodo-62326",
   storageBucket: "speechtodo-62326.firebasestorage.app",
   messagingSenderId: "198151845686",
-  appId: "1:198151845686:web:27528bedcd12b0755c6597",
-  measurementId: "G-MG9S6RG5B1"
+  appId: "1:198151845686:web:27528bedcd12b0755c6597"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
